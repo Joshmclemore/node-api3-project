d4860bfeaab448a5eb48d34b2adf9ef37872609f
Joshmclemore <josh_mclemore@yahoo.com>
2022-04-13 15:57:35 -0500

    validate user and validateuserid are both working

--- api/server.js
+++ api/server.js
@@ -4,12 +4,14 @@ const { logger } = require('./middleware/middleware')
 
 const server = express();
 
+const usersRouter = require('./users/users-router')
+
 // remember express by default cannot parse JSON in request bodies
 server.use(express.json())
 // global middlewares and the user's router need to be connected here
 server.use(logger)
 
-
+server.use('/api/users', usersRouter);
 
 
 server.get('/', (req, res) => {
