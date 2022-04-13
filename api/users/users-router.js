0c4c5d3b6463e9e691815757ea9afcf161614963
Joshmclemore <josh_mclemore@yahoo.com>
2022-04-13 17:23:04 -0500

    passing all tests

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -59,15 +59,27 @@ router.delete('/:id', validateUserId, async (req, res, next) => {
     }
 });
 
-router.get('/:id/posts', validateUserId, (req, res) => {
-  // RETURN THE ARRAY OF USER POSTS
-  // this needs a middleware to verify user id
+router.get('/:id/posts', validateUserId, async (req, res, next) => {
+  try {
+    const result = await User.getUserPosts(req.params.id)
+    res.json(result)
+  }
+    catch (err) {
+    next(err) 
+    }
 });
 
-router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
+router.post('/:id/posts', validateUserId, validatePost, async (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
+  try {
+    const result = await Post.insert({
+      user_id: req.params.id,
+      text: req.text,
+    })
+    res.status(201).json(result)
+  } catch (err) {
+    next(err)
+  }
   console.log(req.user)
   console.log(req.text)
 });
