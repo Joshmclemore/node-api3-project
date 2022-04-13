829ffb497b4616ae1839166c53477ab371e85250
Joshmclemore <josh_mclemore@yahoo.com>
2022-04-13 15:31:08 -0500

    added logger middleware

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,35 @@
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  const timestamp = new Date().toLocaleString()
+  const method = req.method
+  const url = req.originalUrl
+  console.log(`[${timestamp}] ${method} to ${url}`)
+  next()
+  
 }
 
 function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+console.log('validateUserId mmiddleware')
+next()
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+console.log('validateUser mmiddleware')
+next()
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+console.log('validatePost mmiddleware')
+next()
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  validateUserId,
+  validateUser,
+  validatePost,
+  logger
+}
